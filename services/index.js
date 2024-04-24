cc823df0ecb155a1d30b04e66d98719ff287e7fe
Tran Kiet <kiettranuit@gmail.com>
2024-04-24 10:45:15 +0700

    feat: read environment variable

--- services/index.js
+++ services/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
-const host = '127.0.0.1'
-const port = 8181
 const app = express()
+
+require('dotenv').config()
+const host = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || '8282'
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.listen(port, (req, res) => {
-    console.log('Listening on port 8181')
+    console.log(`Server is listening on ${port}`)
 })
